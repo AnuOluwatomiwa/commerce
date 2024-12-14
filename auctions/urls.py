9ff12f01b2268e845c7f91daba1b8b20301dab22
Israel <bamideleisrael04@gmail.com>
2024-12-14 08:30:16 +0100

    Added paths to categories and category listings; putting into consideration <str:category_name> after the backslash

--- auctions/urls.py
+++ auctions/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("form", views.forms_view, name="form"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category_name>", views.category_listings, name="category_listings"),
     path("listing/<int:listing_id>", views.listing_page, name="listing"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("login", views.login_view, name="login"),
